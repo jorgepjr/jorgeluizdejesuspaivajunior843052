038c3166204c7f7282edee54ffab621779a7b29a
jorgepjr <jorgepaiva1806@gmail.com>
2026-01-20 22:10:54 -0400

    chore: Iniciar estrutura do projeto

---
+++ src/main/java/com/musiccatalog/MusiccatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.musiccatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiccatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musiccatalog/MusiccatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiccatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
