83f10d2fb0150087f33186ccb9a79aa581cab061
jorgepjr <jorgepaiva1806@gmail.com>
2026-01-28 20:27:43 -0400

    feat: cria model Artista

--- src/main/java/com/musiccatalog/model/Artista.java
+++ src/main/java/com/musiccatalog/model/Artista.java
@@ -1,8 +1,34 @@
 package com.musiccatalog.model;
 
+import com.musiccatalog.enums.TipoArtista;
+import jakarta.persistence.*;
+
+@Entity
 public class Artista {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String nome;
-    private String tipo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private TipoArtista tipo;
+
+    protected Artista(){}
+
+    public Artista(String nome, TipoArtista tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public TipoArtista getTipo() {return tipo;}
+
+    public void setTipo(TipoArtista tipo) {this.tipo = tipo;}
 }
