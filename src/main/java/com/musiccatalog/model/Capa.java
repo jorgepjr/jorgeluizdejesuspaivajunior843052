a46a7b5efd82ad6d00a9cb918bfc55f17daa6b72
jorgepjr <jorgepaiva1806@gmail.com>
2026-02-02 23:30:57 -0400

    feat: implementa modelagem da capa

--- src/main/java/com/musiccatalog/model/Capa.java
+++ src/main/java/com/musiccatalog/model/Capa.java
@@ -1,12 +1,37 @@
 package com.musiccatalog.model;
 
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
 
+@Entity
 public class Capa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String imagemUrl;
 
-    @JoinColumn(name = "album_id")
+    @Column(name = "arquivo_hash", nullable = false)
+    private String arquivoHash;
+
+    @ManyToOne
+    @JoinColumn(name = "album_id", nullable = false)
     private Album album;
+
+    protected Capa(){}
+
+    public Capa(Album album, String arquivoHash){
+        this.album = album;
+        this.arquivoHash = arquivoHash;
+    }
+
+    public String getArquivoHash() {return arquivoHash;}
+
+    public void setArquivoHash(String arquivoHash) {this.arquivoHash = arquivoHash;}
+
+    public Album getAlbum() {return album;}
+
+    public void setAlbum(Album album) {this.album = album;}
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
 }
