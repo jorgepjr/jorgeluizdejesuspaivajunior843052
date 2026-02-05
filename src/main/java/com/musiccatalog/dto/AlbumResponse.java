cf4746d0149f8ad24da28dfb515c8b5f77f27861
jorgepjr <jorgepaiva1806@gmail.com>
2026-02-05 19:06:07 -0400

    chore: adiciona documentaaco basica no swagger

--- src/main/java/com/musiccatalog/dto/AlbumResponse.java
+++ src/main/java/com/musiccatalog/dto/AlbumResponse.java
@@ -1,7 +1,10 @@
 package com.musiccatalog.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(description = "Retorno da criação do album")
 public record AlbumResponse(
         Long id,
         String nome,
